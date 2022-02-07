15d99c2558326a5bb3b51aa221092dfc779aec0e
Jesper <jesper.glas@gmail.com>
2022-02-07 14:08:29 +0100

    Fixed colors in PauseMenu, still can't exit it.

--- src/PauseMenu.cpp
+++ src/PauseMenu.cpp
@@ -3,19 +3,20 @@
 /* === Private functions === */
 void PauseMenu::initButtons()
 {
-    
+
 }
 
 /* === Constructors === */
 PauseMenu::PauseMenu(sf::RenderWindow &window)
 {
+    this->m_background.setPosition(0.f, 0.f);
     this->m_background.setSize(
         sf::Vector2f(
             window.getSize().x,
             window.getSize().y
         )
     );
-    this->m_background.setFillColor(sf::Color(20, 20, 20, 100));
+    this->m_background.setFillColor(sf::Color(69, 69, 69, 100));
 
     this->m_container.setSize(
         sf::Vector2f(
@@ -23,7 +24,8 @@ PauseMenu::PauseMenu(sf::RenderWindow &window)
             window.getSize().y
         )
     );
-    this->m_background.setFillColor(sf::Color(20, 20, 20, 200));
+    this->m_container.setFillColor(sf::Color(36, 36, 36, 255));
+    this->m_container.setPosition(0.f, 0.f);
 }
 
 /* === Deconstructors === */
